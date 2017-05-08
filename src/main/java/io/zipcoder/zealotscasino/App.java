43cc5f606298361bfad9d54923ad35ea9eeab830
Aaron Long <aaron.long.c@gmail.com>
2017-05-08 16:44:54 -0400

    Origin

---
+++ src/main/java/io/zipcoder/zealotscasino/App.java
@@ -0,0 +1,7 @@
+package io.zipcoder.zealotscasino;
+
+/**
+ * Created by aaronlong on 5/8/17.
+ */
+public class App {
+}
---
+++ src/test/java/io/zipcoder/zealotscasino/AppTest.java
@@ -0,0 +1,12 @@
+package io.zipcoder.zealotscasino;
+
+import org.junit.Test;
+
+/**
+ * Created by aaronlong on 5/8/17.
+ */
+public class AppTest {
+    @Test
+    public void testApp() {
+    }
+}
