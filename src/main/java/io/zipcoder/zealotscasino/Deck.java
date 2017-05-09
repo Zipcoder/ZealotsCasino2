f31a9664f22a6f3425342063e2666f4154282cb8
Andrew Wong <wong.andrew.p@gmail.com>
2017-05-09 15:55:22 -0400

    buildDeck method is tested and works, FIRST TRY MF

--- src/main/java/io/zipcoder/zealotscasino/Deck.java
+++ src/main/java/io/zipcoder/zealotscasino/Deck.java
@@ -1,7 +1,35 @@
 package io.zipcoder.zealotscasino;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by andrewwong on 5/9/17.
  */
 public class Deck {
+    private ArrayList<Card> deckQue = new ArrayList<>();
+    public void shuffle(){
+        Collections.shuffle(deckQue);
+    }
+
+    public ArrayList<Card> getDeckQue() {
+        return deckQue;
+    }
+
+    public void buildDeck(){
+        for(Suit suit: Suit.values()){
+            for(NumberValue numberValue: NumberValue.values()){
+                Card card = new Card(numberValue.getVal(), suit.getVal());
+                deckQue.add(card);
+            }
+            for(FaceValue faceValue: FaceValue.values()){
+                Card card = new Card(faceValue.getVal(), suit.getVal());
+                deckQue.add(card);
+            }
+        }
+    }
+//    public Card getCard(){
+//        Card card;
+//        return card;
+//    }
 }
--- src/test/java/io/zipcoder/zealotscasino/TestDeck.java
+++ src/test/java/io/zipcoder/zealotscasino/TestDeck.java
@@ -1,15 +1,43 @@
 package io.zipcoder.zealotscasino;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by andrewwong on 5/9/17.
  */
 public class TestDeck {
+    Deck deck;
+    @Before
+    public void instantiateDeck(){
+        deck = new Deck();
+    }
     @Test
     public void buildDeck_DeckIsEmpty_DeckOf52Returned(){
         //Given
+        int expectedDeckSize = 52;
+
         //When
+        deck.buildDeck();
 
         //Then
+        assertEquals("Deck is not the correct size", expectedDeckSize, deck.getDeckQue().size());
     }
+    @Test
+    public void buildDeck_DeckIsEmpty_DeckOfUniqueCardReturned(){
+        //Given
+        //When
+        //Then
+    }
+    @Test
+    public void getCard_DeckHasCards_CardIsReturned(){
+        //Given
+        //When
+        //Then
+    }
+
 }
