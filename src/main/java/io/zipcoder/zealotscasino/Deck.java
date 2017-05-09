b81fce9a49718354a0bd1d4087caca3774309e00
Andrew Wong <wong.andrew.p@gmail.com>
2017-05-09 15:28:20 -0400

    Made numbervalue enum

---
+++ src/main/java/io/zipcoder/zealotscasino/Deck.java
@@ -0,0 +1,7 @@
+package io.zipcoder.zealotscasino;
+
+/**
+ * Created by andrewwong on 5/9/17.
+ */
+public class Deck {
+}
---
+++ src/test/java/io/zipcoder/zealotscasino/TestDeck.java
@@ -0,0 +1,15 @@
+package io.zipcoder.zealotscasino;
+
+import org.junit.Test;
+/**
+ * Created by andrewwong on 5/9/17.
+ */
+public class TestDeck {
+    @Test
+    public void buildDeck_DeckIsEmpty_DeckOf52Returned(){
+        //Given
+        //When
+
+        //Then
+    }
+}
