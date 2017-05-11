9a8dd1ef86995fc1568f90725678258bcfb217eb
Chris <chriskim80@gmail.com>
2017-05-11 16:25:52 -0400

    Removed value constructor from Card, added getValue and test

--- src/test/java/io/zipcoder/zealotscasino/TestHand.java
+++ src/test/java/io/zipcoder/zealotscasino/TestHand.java
@@ -16,7 +16,7 @@ public class TestHand {
     @Before
     public void setUp(){
         expected = new ArrayList<>();
-        Card[] cards = { new Card(9, Card.Suit.SPADES.name()), new Card(10, Card.Suit.CLUBS.name()) };
+        Card[] cards = { new Card("NINE", Card.Suit.SPADES.name()), new Card("TEN", Card.Suit.CLUBS.name()) };
         hand = new Hand();
         for (Card card : cards) { expected.add(card); }
     }
